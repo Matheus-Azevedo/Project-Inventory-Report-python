bd248ecc9ac2af80f9ae9e479cd49352dd2419e3
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-05 01:35:11 -0300

    feat: importando CSV e JSON files

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,44 @@
+import os.path
+import csv
+import json
+import xml.etree.ElementTree as ET
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory():
+    @staticmethod
+    def import_data(file_path: str, type: str) -> list[dict]:
+        file_extension = os.path.splitext(file_path)[1].lower()
+        if file_extension == ".csv":
+            inventory = Inventory.import_csv(file_path)
+        elif file_extension == ".json":
+            inventory = Inventory.import_json(file_path)
+        elif file_extension == ".xml":
+            inventory = Inventory.import_xml(file_path)
+        else:
+            raise ValueError("Arquivo inválido")
+
+        if type == "simples":
+            return SimpleReport.generate(inventory)
+        elif type == "completo":
+            return CompleteReport.generate(inventory)
+        else:
+            raise ValueError("Tipo inválido")
+
+    @staticmethod
+    def import_csv(path: str) -> list[dict]:
+        with open(path, "r") as file:
+            reader = csv.DictReader(file)
+            return [row for row in reader]
+
+    @staticmethod
+    def import_json(path: str) -> list[dict]:
+        with open(path, "r") as file:
+            return json.load(file)
+
+    @staticmethod
+    def import_xml(path: str) -> list[dict]:
+        with open(path, "r") as file:
+            root = ET.parse(file).getroot()
+            return [dict(child.attrib) for child in root]
