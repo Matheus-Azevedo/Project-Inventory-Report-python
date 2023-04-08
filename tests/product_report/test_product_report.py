6b049dfaff84379e6d6ca5c3482cda6928961910
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-08 13:21:44 -0300

    feat: teste para product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product('1',
+                      'Arroz',
+                      'Arroz da Trybe',
+                      '2022-01-01',
+                      '2023-01-01',
+                      '123456789',
+                      'em local seco')
+    product_string = str(product)
+    assert 'Arroz' in product_string
+    assert 'Arroz da Trybe' in product_string
+    assert '2022-01-01' in product_string
+    assert '2023-01-01' in product_string
+    assert 'em local seco' in product_string
